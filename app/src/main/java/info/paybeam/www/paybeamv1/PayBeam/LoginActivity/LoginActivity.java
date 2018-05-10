f7c24b89893199b878a86424ee77c203b99a7651
z1980s <zicokuang@hotmail.com>
2018-05-10 16:20:18 +0800

    Home screen interface changes

--- app/src/main/java/info/paybeam/www/paybeamv1/PayBeam/LoginActivity/LoginActivity.java
+++ app/src/main/java/info/paybeam/www/paybeamv1/PayBeam/LoginActivity/LoginActivity.java
@@ -55,8 +55,8 @@ public class LoginActivity extends AppCompatActivity implements LoginContract.Lo
 
 
         //For testing purposes we assume successful login and call menu here
-        //Intent intent = new Intent(this, HomeActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
 
 
     }
